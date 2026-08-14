ba9849e4a697bea65986fe890d6a8557af9a5a47
OtavioMomberg <otavioaugusto777444@gmail.com>
2026-08-14 11:11:09 -0300

    update: Documentation added to the public API

--- lib/custom_feedback.dart
+++ lib/custom_feedback.dart
@@ -4,7 +4,23 @@ import 'package:custom_feedback/src/custom_snackbar.dart';
 import 'package:custom_feedback/src/models/dialog_model.dart';
 import 'package:custom_feedback/src/models/snackbar_model.dart';
 
+/// The [CustomFeedback] is the main class where 
+/// its possible to call [dialog] and [snackBar] methods
 class CustomFeedback {
+
+  /// Displays a customizable [AlertDialog].
+  ///
+  /// The generic type [T] represents the value returned 
+  /// when the dialog is closed.
+  /// 
+  /// The [onOpen] callback is executed after the dialog is rendered.
+  /// When [onOpen] is provided, [closeTime] is ignored.
+  ///
+  /// If [closeTime] is provided, [barrierDismissible] 'should' be set to
+  /// `false` to prevent the dialog from being dismissed manually.
+  ///
+  /// When [closeIcon] is `true`, a close button is displayed next
+  /// to the [title].
   static Future<T?> dialog<T>({
     required BuildContext context,
     Widget title = const Text("Dialog"),
@@ -21,6 +37,11 @@ class CustomFeedback {
     MainAxisAlignment? actionsAlignment,
     EdgeInsetsGeometry? actionsPadding,
     List<Widget>? actions,
+    /// A callback executed after the dialog is rendered.
+    ///
+    /// The dialog remains open while the returned [Future] is being completed.
+    /// Once the callback completes, its return value is used as the dialog
+    /// result and the dialog is closed.
     Future<T?> Function()? onOpen,
   }) {
 
@@ -43,6 +64,10 @@ class CustomFeedback {
     return CustomDialog.dialog(context: context, dialogModel: dialogModel);
   }
 
+  /// Displays a customizable [SnackBar].
+  ///
+  /// The appearance and behavior of the [SnackBar] can be customized
+  /// through the provided parameters.
   static void snackBar({
     required BuildContext context,
     Widget content = const Text("SnackBar Content"),
